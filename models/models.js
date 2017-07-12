5435b74a8082887ffbc7eeeb23ea849dc7272238
SSantiago90 <salkin.santiago@gmail.com>
2017-07-11 21:55:23 -0300

    Revert "Dif de versiones local/heroku con PG"
    This reverts commit 0e582591346ea1287aea7543139cbb22b07979b5.

--- models/models.js
+++ models/models.js
@@ -1,40 +1,21 @@
 var path = require('path');
 
-//definir local/dev enviroment y corresp. DB
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name = (url[6] || null);
-var user = (url[2] || null);
-var pwd = (url[3] || null);
-var protocol = (url[1] || null);
-var dialect = (url[1] || null);
-var port = (url[5] || null);
-var host = (url[4] || null);
-var storage= procces.env.DATABASE_STORAGE;
-
 //carga de ORM y preconfig de BBDD
 var Sequelize = require ('sequelize');
-var sequielize = newSequelize(DB_name, user, pwd, 
-				{ dialect:  dialect,
-				  protocol: protocol, 
-				  port: 	port,
-				  host: 	host,
-				  storage:  storage, //solo SQLITE (.env)
-				  omitNull: true 	 //solo PG
-				}
-);
+var sequelize = new Sequelize(null,null,null,{dialect:'sqlite',storage:'quiz.sqlite'});
 
-//importar y exportar estructura modelo de tabla de /models
+//importar y exportar estructura modelo de tabla
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
 exports.Quiz = Quiz;
 
 //Inicializar BBDD
 sequelize.sync().success(function(){
-	Quiz.count().success(function(count){
+	Quiz.count().succes(function(count){
 		Quiz.create({
 			pregunta: 'Capital de Argentina',
-			respuesta: 'buenos aires'
-		}).success(function(){console.log('DB incializada correctamente')});		
+			respuesta: 'Buenos Aires'
+		}).succes(function(){console.log('DB incializada correctamente')});
+		}
 	});
 });
 
