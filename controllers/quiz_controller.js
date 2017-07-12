677ba8718da319f8f295320a35cbacfe4d8242a1
SSantiago90 <salkin.santiago@gmail.com>
2017-07-11 22:53:56 -0300

    solving deploy pg/heroku issues

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,19 @@
-var models = require ('../models/models.js');
+var models = require('../models/models.js');
 //GET question
 exports.question = function(req,res){
 	models.Quiz.findAll().success(function(quiz){
 	res.render('quizes/question',{pregunta: quiz[0].pregunta})
+	});
 };
 //GET answer
 exports.answer = function(req,res){
-	models.Quiz.findAll.succes(function(quiz){
-		if (req.query.respuesta.toLowerCase() === pregunta[0].respuesta){
+	models.Quiz.findAll().success(function(quiz){
+		if (req.query.respuesta.toLowerCase() === quiz[0].respuesta){
 			res.render('quizes/answer',{respuesta: "Correcto!"});
 		} 
 		else{
+			console.log(quiz[0].respuesta);
 			res.render('quizes/answer',{respuesta: "Inorrecto :("});
 		}
-	})	
+	});	
 };
