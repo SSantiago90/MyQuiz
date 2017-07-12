8ba8791b00996ae5983e9b2a138f0f109fbf7d82
SSantiago90 <salkin.santiago@gmail.com>
2017-07-11 22:00:59 -0300

    Revert "Revert "Dif de versiones local/heroku con PG""
    This reverts commit 5435b74a8082887ffbc7eeeb23ea849dc7272238.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,16 +2,17 @@ var models = require ('../models/models.js');
 //GET question
 exports.question = function(req,res){
 	models.Quiz.findAll().success(function(quiz){
-	res.render('quizes/question',{pregunta: quiz[0].pregunta})
-});
+		res.render('quizes/question',{pregunta: quiz[0].pregunta})
+	})
+};	
 //GET answer
 exports.answer = function(req,res){
-	models.Quiz.findAll.succes(function(quiz){
-		if (req.query.respuesta.toLowerCase() === pregunta[0].respuesta){
+	models.Quiz.findAll().success(function(quiz){
+		if (req.query.respuesta.toLowerCase() === quiz[0].respuesta){
 			res.render('quizes/answer',{respuesta: "Correcto!"});
-		} 
-		else{
-			res.render('quizes/answer',{respuesta: "Inorrecto :("});
 		}
-	})	
-};
+		else{
+			res.render('quizes/answer',{respuesta: "Incorrecto :("});
+		}
+	})
+}
